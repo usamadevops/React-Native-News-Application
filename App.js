b48c8aa74eed2a0307123d9d9932c0b3ac2d3efd
usamaltaf <ualtaf238@gmail.com>
2021-10-19 19:05:30 +0500

    amplify added

--- App.js
+++ App.js
@@ -3,8 +3,12 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import MainStack from './Source/navigation/AuthStack';
 import stores from './Source/redux/store';
-import {StatusBar, View} from 'react-native';
+import { StatusBar, View } from 'react-native';
+import Amplify from 'aws-amplify';
+import awsmobile from './src/aws-exports'
+Amplify.configure(awsmobile);
 const App = () => {
+  console.log(Amplify.configure(awsmobile))
   return (
     <Provider store={stores}>
       <View style={{flex: 1}}>
