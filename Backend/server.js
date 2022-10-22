7518de775890a4199bea27181d5d7a795db374d9
Usama Altaf <ualtaf238@gmail.com>
2022-10-22 14:02:55 +0500

    Removed Backend

--- Backend/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express'); //requiring express to use it
-const app = express();
-app.listen(3000, function () {
-  console.log('listening on 3000');
-});
-// app.get(endpoint, callback);
-app.get('/', function (req, res) {
-  res.send('Hello World');
-});
