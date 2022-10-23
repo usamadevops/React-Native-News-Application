ee6b9c2bad3790b003be2fe085c470926a259eac
Usama Altaf <ualtaf238@gmail.com>
2022-10-23 19:00:56 +0500

    Action Sheet added

--- Source/Components/Filterpopup.js
+++ Source/Components/Filterpopup.js
@@ -1,22 +1,22 @@
-import { View, Text,StyleSheet ,Dimensions} from 'react-native'
+import { View, Text, StyleSheet, Dimensions } from 'react-native'
 import React from 'react';
-import  {Gesture,GestureDetector } from 'react-native-gesture-handler';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 const { height: SCREEN_HEIGHT } = Dimensions.get('window');
 import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
-const Filterpopup = ({translateY}) => {
+const Filterpopup = ({ translateY }) => {
     const context = useSharedValue(0);
     const gesture = Gesture.Pan().onStart(() => {
         context.value = { y: translateY.value };
     }).onUpdate((e) => {
         translateY.value = e.translationY + context.value.y;
-        translateY.value=Math.max (translateY.value,-SCREEN_HEIGHT/1.4);
+        translateY.value = Math.max(translateY.value, -SCREEN_HEIGHT / 1.4);
 
-    }).onEnd(()=>{
-        if(translateY.value > -SCREEN_HEIGHT/1.3){
-            translateY.value=withSpring(0,{damping:50});
+    }).onEnd(() => {
+        if (translateY.value > -SCREEN_HEIGHT / 1.3) {
+            translateY.value = withSpring(0, { damping: 50 });
         }
-        else{
-            translateY.value=translateY.value;
+        else {
+            translateY.value = translateY.value;
         }
     });
 
@@ -27,38 +27,38 @@ const Filterpopup = ({translateY}) => {
     });
 
 
-    
-  return (
-    <GestureDetector gesture={gesture} >
 
-  <Animated.View style={[styles.bottomSheet, rBottomSheetStyle]}>
-  <View style={styles.line} />
-                
+    return (
+        <GestureDetector gesture={gesture} >
+
+            <Animated.View style={[styles.bottomSheet, rBottomSheetStyle]}>
+                <View style={styles.line} />
+
             </Animated.View>
-    </GestureDetector>
-  )
+        </GestureDetector>
+    )
 }
 
 export default Filterpopup;
 
-const styles=StyleSheet.create({
+const styles = StyleSheet.create({
     bottomSheet: {
-    
+
         height: '100%',
         width: '100%',
         backgroundColor: "#fff",
         position: 'absolute',
-        top:SCREEN_HEIGHT,
-        elevation:-20,
-        shadowOpacity:0.8,
-        shadowColor:'#00000040',
-        shadowOffset:{
-            width:5,
-            height:-5,
+        top: SCREEN_HEIGHT,
+        elevation: -20,
+        shadowOpacity: 0.8,
+        shadowColor: '#00000040',
+        shadowOffset: {
+            width: 5,
+            height: -5,
         },
         borderRadius: 20,
         zIndex: 1000,
-        
+
     },
     line: {
         width: 75,
